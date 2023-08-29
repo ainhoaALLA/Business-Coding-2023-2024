52232efd7578c7a8808ba0a31393d29572021724
ainhoaALLA <ainhoa.fernandez.ruiz@gmail.com>
2023-08-29 23:25:44 +0200

    Adding window events exercises

---
+++ 4-JAVASCRIPT/4.2-JS_DOM/03-Window_events/main.js
@@ -0,0 +1,36 @@
+const onCompleteGame = () => {
+  alert("Juego completado!");
+};
+
+const buttonElement = document.querySelector("#complete-game");
+buttonElement.addEventListener("click", onCompleteGame);
+
+const pTextList = [];
+
+const onMouseMove = (event) => {
+  if (event.target.tagName === "P") {
+    if (!pTextList.includes(event.target.className)) {
+      pTextList.push(event.target.className);
+
+      if (pTextList.length === 5) {
+        document.body.style.height = "200vh";
+      }
+    }
+  }
+};
+
+let enteredPassword = false;
+
+const onScroll = () => {
+  if (!enteredPassword && window.scrollY > window.outerHeight / 2) {
+    const password = prompt("Introduce la contraseña:");
+
+    if (password === "1234") {
+      buttonElement.style.display = "block";
+      enteredPassword = true;
+    }
+  }
+};
+window.addEventListener("mousemove", onMouseMove);
+
+window.addEventListener("scroll", onScroll);
